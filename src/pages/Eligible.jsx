03312c53b7858bcc9533146cc340f7cdc9faacdc
SoftLife0 <onikosiadewale18@gmail.com>
2024-05-28 18:54:46 +0000

    form validation

--- src/pages/Eligible.jsx
+++ src/pages/Eligible.jsx
@@ -17,40 +17,37 @@ const Eligible = () => {
     const name = data?.data?.name || '';
     const eligibleCourses = data?.data?.eligibleCourses || [];
 
+    const headingStyle = {
+        fontFamily: 'Poppins, sans-serif',
+        fontWeight: '700',
+        textDecoration: 'none',
+        color: '#fff',
+        fontSize: '20px',
+    };
 
-  const headingStyle = {
-    fontFamily: 'Poppins, sans-serif',
-    letterSpacing: '0.04em',
-    fontWeight:'700',
-    textDecoration: 'none',
-    color: '#fff', 
-    fontSize: '20px', 
-  };
-
-  const subHeading = {
-    fontSize:'16px', 
-    letterSpacing: '0.05em',
-    fontFamily: 'Poppins, sans-serif',
-    margin:'5px 0',
-    fontWeight:'500',
-    color: '#fff'
-  }
+    const subHeading = {
+        fontSize: '16px',
+        fontFamily: 'Poppins, sans-serif',
+        margin: '5px 0',
+        fontWeight: '500',
+        color: '#fff'
+    };
 
     return (
-        <>  
-            <div style={{ position: 'fixed', width: '100%', zIndex: '100', top: '0', backgroundColor: '#DE2121', borderBottom: '1px solid #dc3545' }}>
+        <>
+            <div style={{ position: 'fixed', width: '100%', height:'auto', zIndex: '100', top: '0', backgroundColor: '#DE2121', borderBottom: '1px solid #dc3545' }}>
                 <header className="navbar navbar-expand-lg navbar-light bg-light" style={{ padding: '16px 16px' }}>
-                    <a className="navbar-brand" href="/" style={headingStyle}>Eligiblity Checker</a>
+                    <a className="navbar-brand" href="/" style={headingStyle}>Eligibility Checker</a>
 
                     <div style={subHeading}>
-                    <h2>Congratulations <span> {name}</span></h2>
-                    <small style={{fontSize:'16px'}}>You are eligible for the following courses. Pick one to start your application process</small>
+                        <h2 style={{marginTop:'0', marginBottom:'3px'}}>Congratulations <span> {name}</span></h2>
+                        <small style={{ fontSize: '16px' }}>You are eligible for the following courses. Pick one to start your application process</small>
                     </div>
                 </header>
             </div>
-            {/* <Layout name={name}/> */}
-            <Container style={{ marginTop: '25vh', height:'100vh' }}>
 
+            {/* <Layout name={name}/> */}
+            <Container style={{ paddingTop: '25vh', height: '100vh' }}>
                 {/* Check if eligibleCourses is not empty */}
                 {data ? (
                     eligibleCourses.length > 0 ? (
