096b26a38baf2112f11414a36df03bb36302b3f3
SoftLife0 <onikosiadewale18@gmail.com>
2024-06-03 10:48:27 +0000

    dynamic eligible process

--- src/pages/Eligible.jsx
+++ src/pages/Eligible.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-// import Layout from '../components/Layout';
 import { Container } from '@mui/material';
 import CustomCard from '../components/CustomCard';
 
 const Eligible = () => {
-    // Extracting the state from the location object
     const location = useLocation();
     const state = location.state || {};
     const data = state.data;
 
-    // Log the received data
     console.log('Received data:', data);
 
-    // Safely access data properties
     const name = data?.data?.name || '';
     const eligibleCourses = data?.data?.eligibleCourses || [];
+    const isEligible = eligibleCourses.length > 0;
 
     const headingStyle = {
         fontFamily: 'Poppins, sans-serif',
@@ -30,27 +27,34 @@ const Eligible = () => {
         fontFamily: 'Poppins, sans-serif',
         margin: '5px 0',
         fontWeight: '500',
-        color: '#fff'
+        color: '#fff',
     };
 
     return (
         <>
-            <div style={{ position: 'fixed', width: '100%', height:'auto', zIndex: '100', top: '0', backgroundColor: '#DE2121', borderBottom: '1px solid #dc3545' }}>
+            <div style={{ position: 'fixed', width: '100%', height: 'auto', zIndex: '100', top: '0', backgroundColor: '#DE2121', borderBottom: '1px solid #dc3545' }}>
                 <header className="navbar navbar-expand-lg navbar-light bg-light" style={{ padding: '16px 16px' }}>
                     <a className="navbar-brand" href="/" style={headingStyle}>Eligibility Checker</a>
-
+                    
                     <div style={subHeading}>
-                        <h2 style={{marginTop:'0', marginBottom:'3px'}}>Congratulations <span> {name}</span></h2>
-                        <small style={{ fontSize: '16px' }}>You are eligible for the following courses. Pick one to start your application process</small>
+                        {isEligible ? (
+                            <>
+                                <h2 style={{ marginTop: '0', marginBottom: '3px' }}>Congratulations <span>{name}</span></h2>
+                                <small style={{ fontSize: '16px' }}>You are eligible for the following courses. Pick one to start your application process</small>
+                            </>
+                        ) : (
+                            <>
+                                <h2 style={{ marginTop: '0', marginBottom: '3px' }}>Sorry <span>{name}</span></h2>
+                                <small style={{ fontSize: '16px' }}>You didn't pass the eligibility criteria for any courses. Please try again later or contact support for more information.</small>
+                            </>
+                        )}
                     </div>
                 </header>
             </div>
 
-            {/* <Layout name={name}/> */}
             <Container style={{ paddingTop: '25vh', height: '100vh' }}>
-                {/* Check if eligibleCourses is not empty */}
                 {data ? (
-                    eligibleCourses.length > 0 ? (
+                    isEligible ? (
                         eligibleCourses.map(eligibleCourse => (
                             <CustomCard key={eligibleCourse.name} department={eligibleCourse.department} name={eligibleCourse.name} color={eligibleCourse.color} />
                         ))
