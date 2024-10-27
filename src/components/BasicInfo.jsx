206619a0e7239afe0fdbe0629575d2848545ba23
SoftLife0 <onikosiadewale18@gmail.com>
2024-10-27 13:40:51 +0000

    Refactor Eligibility, creating a new Api service to get data seemlessly

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import { Grid, TextField, MenuItem } from '@mui/material';
+// import React from 'react';
+// import { Grid, TextField, MenuItem } from '@mui/material';
 
-const BasicInfo = ({ name, number, email, courseOffered, courses, handleNameChange, handleNumberChange, handleEmailChange, handleCourseOffered  }) => {
+// const BasicInfo = ({ name, number, email, courseOffered, courses, handleNameChange, handleNumberChange, handleEmailChange, handleCourseOffered  }) => {
 
-    const grid = { paddingTop: '0' };
+//     const grid = { paddingTop: '0' };
 
-    return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} style={grid}>
-                <TextField
-                    label="Full Name"
-                    value={name}
-                    fullWidth
-                    margin="normal"
-                    placeholder="Name"
-                    type="text"
-                    onChange={handleNameChange}
-                    required
-                    size="small"
-                />
-            </Grid>
-            <Grid item xs={12} style={grid}>
-                <TextField
-                    label="Phone number"
-                    value={number}
-                    fullWidth
-                    placeholder="0244500300"
-                    type="number"
-                    margin="normal"
-                    onChange={handleNumberChange}
-                    required
-                    size="small"
-                />
-            </Grid>
-            <Grid item xs={12} style={grid}>
-                <TextField
-                    label="Email Address"
-                    value={email}
-                    fullWidth
-                    placeholder="john@gmail.com"
-                    type="email"
-                    margin="normal"
-                    onChange={handleEmailChange}
-                    required
-                    size="small"
-                />
-            </Grid>
-            <Grid item xs={12} style={grid}>
-                <TextField
-                    select
-                    label="Which course did you offer"
-                    value={courseOffered}
-                    fullWidth
-                    margin="normal"
-                    onChange={handleCourseOffered}
-                    required
-                    size="small"
-                >
-                    {courses.map((course) => (
-                        <MenuItem key={course} value={course}>
-                            {course}
-                        </MenuItem>
-                    ))}
-                </TextField>
-            </Grid>
-        </Grid>
-    );
-};
+//     return (
+//         <Grid container spacing={2}>
+//             <Grid item xs={12} style={grid}>
+//                 <TextField
+//                     label="Full Name"
+//                     value={name}
+//                     fullWidth
+//                     margin="normal"
+//                     placeholder="Name"
+//                     type="text"
+//                     onChange={handleNameChange}
+//                     required
+//                     size="small"
+//                 />
+//             </Grid>
+//             <Grid item xs={12} style={grid}>
+//                 <TextField
+//                     label="Phone number"
+//                     value={number}
+//                     fullWidth
+//                     placeholder="0244500300"
+//                     type="number"
+//                     margin="normal"
+//                     onChange={handleNumberChange}
+//                     required
+//                     size="small"
+//                 />
+//             </Grid>
+//             <Grid item xs={12} style={grid}>
+//                 <TextField
+//                     label="Email Address"
+//                     value={email}
+//                     fullWidth
+//                     placeholder="john@gmail.com"
+//                     type="email"
+//                     margin="normal"
+//                     onChange={handleEmailChange}
+//                     required
+//                     size="small"
+//                 />
+//             </Grid>
+//             <Grid item xs={12} style={grid}>
+//                 <TextField
+//                     select
+//                     label="Which course did you offer"
+//                     value={courseOffered}
+//                     fullWidth
+//                     margin="normal"
+//                     onChange={handleCourseOffered}
+//                     required
+//                     size="small"
+//                 >
+//                     {courses.map((course) => (
+//                         <MenuItem key={course} value={course}>
+//                             {course}
+//                         </MenuItem>
+//                     ))}
+//                 </TextField>
+//             </Grid>
+//         </Grid>
+//     );
+// };
 
-export default BasicInfo;
+// export default BasicInfo;
--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -5,8 +5,8 @@ const LoadingSpinner = ({ message }) => {
     return (
         <div id="loading" style={{ height: '100vh', width: '100vw', display: 'flex', backgroundColor: '#fff', position: 'fixed', top: 0, alignItems: 'center', justifyContent: 'center', zIndex: 9999 }}>
             <div style={{ width: '100%', textAlign: 'center' }}>
-                <CircularProgress style={{ width: '3rem', height: '3rem', color:'#000' }} /> {/* Increase the size of the spinner */}
-                <h5 style={{ textAlign: 'center', marginTop: '2rem' }}>{message}</h5> {/* Add margin-top for the message */}
+                <CircularProgress style={{ width: '3rem', height: '3rem', color:'#000' }} /> 
+                <h5 style={{ textAlign: 'center', marginTop: '2rem' }}>{message}</h5>
             </div>
         </div>
     );
